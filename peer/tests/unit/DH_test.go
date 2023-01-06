1bc351022f13fd73cb5d705989d793561f72d8cb
Jiri Lhotka <jl887@cantab.ac.uk>
2023-01-06 12:04:39 +0100

    move test

--- peer/tests/unit/DH_test.go
+++ peer/tests/unit/DH_test.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/stretchr/testify/require"
 	z "go.dedis.ch/cs438/internal/testing"
-	"go.dedis.ch/cs438/transport"
 	"go.dedis.ch/cs438/transport/channel"
 )
 
@@ -129,47 +128,4 @@ func Test_DH_ThreeNodeSetup(t *testing.T) {
 	require.NotEqual(t, node1.GetSymKey(node3.GetAddr()), node3.GetSymKey(node2.GetAddr()))
 	require.NotEqual(t, node1.GetSymKey(node2.GetAddr()), node1.GetSymKey(node3.GetAddr()))
 
-}
-
-func Test_TLS_SymmetricEncryption(t *testing.T) {
-	transp := channel.NewTransport()
-	fake := z.NewFakeMessage(t)
-	handler1, _ := fake.GetHandler(t)
-	handler2, _ := fake.GetHandler(t)
-
-	publicKeyN1, privateKeyN1 := GenerateKeyPair()
-	node1 := z.NewTestNode(t, peerFac, transp, "127.0.0.1:0", z.WithMessage(fake, handler1), z.WithAntiEntropy(time.Millisecond*50), z.WithKeys(publicKeyN1, privateKeyN1))
-	defer node1.Stop()
-
-	publicKeyN2, privateKeyN2 := GenerateKeyPair()
-	node2 := z.NewTestNode(t, peerFac, transp, "127.0.0.1:0", z.WithMessage(fake, handler2), z.WithAntiEntropy(time.Millisecond*50), z.WithKeys(publicKeyN2, privateKeyN2))
-	defer node2.Stop()
-
-	node1.SetAsmKey(node2.GetAddr(), publicKeyN2)
-	node2.SetAsmKey(node1.GetAddr(), publicKeyN1)
-
-	//node1 <-> node2
-
-	node2.AddPeer(node1.GetAddr())
-	node1.AddPeer(node2.GetAddr())
-	node1.CreateDHSymmetricKey(node2.GetAddr())
-
-	time.Sleep(time.Second)
-	require.Equal(t, node1.GetSymKey(node2.GetAddr()), node2.GetSymKey(node1.GetAddr()))
-
-	// Test Node1 -> Node2
-	messageToEncrypt := []byte("Hello World")
-	encrypted, err := node1.EncryptSymmetric(node2.GetAddr(), transport.Message{Payload: messageToEncrypt})
-	require.NoError(t, err)
-	decrypted, err := node2.DecryptSymmetric(node1.GetAddr(), &encrypted)
-	require.NoError(t, err)
-	require.Equal(t, messageToEncrypt, []byte(decrypted.Payload))
-
-	// Test Node2 -> Node1
-	messageToEncrypt = []byte("Yellow World")
-	encrypted, err = node2.EncryptSymmetric(node1.GetAddr(), transport.Message{Payload: messageToEncrypt})
-	require.NoError(t, err)
-	decrypted, err = node1.DecryptSymmetric(node2.GetAddr(), &encrypted)
-	require.NoError(t, err)
-	require.Equal(t, messageToEncrypt, []byte(decrypted.Payload))
 }
